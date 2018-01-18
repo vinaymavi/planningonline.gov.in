61971eb94b1d1056bf62d18b0bbeb54439027289
VinayMavi <vinaymavi@gmail.com>
2018-01-18 22:35:34 +0530

    Circle test covrage testing.

--- .circleci/test_coverage.sh
+++ .circleci/test_coverage.sh
@@ -3,6 +3,5 @@
 set -e
 
 yarn test --coverage --maxWorkers=2
-if [ -z $CI_PULL_REQUEST ]; then
-  cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
-fi
+pwd 
+cat ./coverage/lcov.info | ./node_modules/.bin/coveralls
